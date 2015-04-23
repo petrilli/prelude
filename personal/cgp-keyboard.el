cc9f5002ec12f7115d9734334157fb64c907e1fc
Christopher Petrilli <petrilli@amber.org>
2015-04-22 20:39:25 -0700

    Keyboard customizations

---
+++ personal/cgp-keyboard.el
@@ -0,0 +1,12 @@
+;;; Customization of the keyboard
+
+;; Mac keyboard
+(when (string= system-type "darwin")
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier 'super))
+
+;; Personal preferences
+(global-set-key [(control w)] 'backward-kill-word)
+(global-set-key [(super w)] 'kill-region)
+
+;;; cgp-keyboard.el ends here
